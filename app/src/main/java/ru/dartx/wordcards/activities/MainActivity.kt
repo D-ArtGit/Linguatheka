8902cba58b72c838042141b09c3f83550405acce
D-ArtGit <dartx79@gmail.com>
2022-03-25 19:07:02 +0300

    1

---
+++ app/src/main/java/ru/dartx/wordcards/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.dartx.wordcards.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.dartx.wordcards.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/ru/dartx/wordcards/entities/Card.kt
@@ -0,0 +1,23 @@
+package ru.dartx.wordcards.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "cards")
+data class Card(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    @ColumnInfo(name = "word")
+    val word: String,
+    @ColumnInfo(name = "content")
+    val content: String,
+    @ColumnInfo(name = "createDate")
+    val createDate: String,
+    @ColumnInfo(name = "createTime")
+    val createTime: String,
+    @ColumnInfo(name = "remindDate")
+    val remindDate: String,
+    @ColumnInfo(name = "remindTime")
+    val remindTime: String
+)
