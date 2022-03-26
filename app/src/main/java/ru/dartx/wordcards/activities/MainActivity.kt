c0768f60153ed2b3779070abe85de36463c5744b
D-ArtGit <dartx79@gmail.com>
2022-03-26 15:39:23 +0300

    3

--- app/src/main/java/ru/dartx/wordcards/activities/MainActivity.kt
+++ app/src/main/java/ru/dartx/wordcards/activities/MainActivity.kt
@@ -3,10 +3,13 @@ package ru.dartx.wordcards.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.dartx.wordcards.R
+import ru.dartx.wordcards.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/ru/dartx/wordcards/db/Dao.kt
+++ app/src/main/java/ru/dartx/wordcards/db/Dao.kt
@@ -1,7 +1,20 @@
 package ru.dartx.wordcards.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import ru.dartx.wordcards.entities.Card
 
 @Dao
 interface Dao {
+    @Query("SELECT * FROM cards")
+    fun getAllCards(): Flow<List<Card>>
+    @Query("DELETE FROM cards WHERE id IS :id")
+    suspend fun deleteCard(id: Int)
+    @Insert
+    suspend fun insertCard(card: Card)
+    @Update
+    suspend fun updateCard(card: Card)
 }
