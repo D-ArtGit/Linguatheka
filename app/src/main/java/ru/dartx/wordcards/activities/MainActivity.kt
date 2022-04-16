92f5f2a549625f8542e10ff7aa6da62d20087ecd
D-ArtGit <dartx79@gmail.com>
2022-04-16 10:03:53 +0300

    6

--- app/src/main/java/ru/dartx/wordcards/activities/MainActivity.kt
+++ app/src/main/java/ru/dartx/wordcards/activities/MainActivity.kt
@@ -2,14 +2,42 @@ package ru.dartx.wordcards.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
 import ru.dartx.wordcards.R
 import ru.dartx.wordcards.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var edSearch: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        val search = menu!!.findItem(R.id.search)
+        edSearch = search.actionView.findViewById(R.id.edSearch) as EditText
+        search.setOnActionExpandListener(expandActionView())
+        return true
+    }
+
+    private fun expandActionView(): MenuItem.OnActionExpandListener {
+        return object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                invalidateOptionsMenu()
+                return true
+            }
+
+
+        }
     }
 }
