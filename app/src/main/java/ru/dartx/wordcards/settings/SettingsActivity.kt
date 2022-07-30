12fbd817ca0b76592d85d9c78b05554f4ba27958
D-ArtGit <dartx79@gmail.com>
2022-07-30 08:42:04 +0300

    52

--- app/src/main/java/ru/dartx/wordcards/settings/SettingsActivity.kt
+++ app/src/main/java/ru/dartx/wordcards/settings/SettingsActivity.kt
@@ -2,9 +2,11 @@ package ru.dartx.wordcards.settings
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import ru.dartx.wordcards.R
@@ -23,32 +25,34 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        defPreference.registerOnSharedPreferenceChangeListener { _, key ->
-            if (key == "theme") {
-                setTheme(getSelectedTheme())
-                recreate()
-            } else if (key == "night_mode") {
-                AppCompatDelegate.setDefaultNightMode(getSelectedNightMode())
-            }
-
-        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
-//            val nightMode: Preference? = findPreference("night_mode")
-//            nightMode?.setOnPreferenceChangeListener { _, value ->
-//                Log.d("DArtX", "NightMode: ${value as String}")
-//                AppCompatDelegate.setDefaultNightMode(
-//                    when (value as String) {
-//                        "day" -> AppCompatDelegate.MODE_NIGHT_NO
-//                        "night" -> AppCompatDelegate.MODE_NIGHT_YES
-//                        else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-//                    }
-//                )
-//                true
-//            }
+            val nightMode: Preference? = findPreference("night_mode")
+            val themeMode: Preference? = findPreference("theme")
+            nightMode?.setOnPreferenceChangeListener { _, value ->
+                AppCompatDelegate.setDefaultNightMode(
+                    when (value as String) {
+                        "day" -> AppCompatDelegate.MODE_NIGHT_NO
+                        "night" -> AppCompatDelegate.MODE_NIGHT_YES
+                        else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    }
+                )
+                true
+            }
+            themeMode?.setOnPreferenceChangeListener { _, value ->
+                activity?.setTheme(
+                    when (value as String) {
+                        "blue" -> R.style.Theme_WordCardsBlue
+                        "green" -> R.style.Theme_WordCardsGreen
+                        else -> R.style.Theme_WordCardsRed
+                    }
+                )
+                activity?.recreate()
+                true
+            }
         }
     }
 
@@ -65,17 +69,6 @@ class SettingsActivity : AppCompatActivity() {
             "blue" -> R.style.Theme_WordCardsBlue
             "green" -> R.style.Theme_WordCardsGreen
             else -> R.style.Theme_WordCardsRed
-        }
-    }
-
-    private fun getSelectedNightMode(): Int {
-        return when (defPreference.getString(
-            "night_mode",
-            "system"
-        )) {
-            "day" -> AppCompatDelegate.MODE_NIGHT_NO
-            "night" -> AppCompatDelegate.MODE_NIGHT_YES
-            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
         }
     }
 }
